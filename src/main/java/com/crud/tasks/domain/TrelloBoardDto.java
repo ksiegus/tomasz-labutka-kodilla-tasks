3e7e5012759bf64c081140f6a7595d4ee4c82f8b
ksiegus <tomek@labutka.pl>
2021-02-13 23:19:13 +0100

    22.3. Rozbudowa klienta

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,11 +1,15 @@
 package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrelloBoardDto {
     @JsonProperty("id")
     private String id;
